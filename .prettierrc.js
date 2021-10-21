3328de3e0a885dd4d4df380477c88f2d6a439e13
Pooya Tolideh <ptolideh@pm.me>
2021-10-20 23:30:20 -0400

    tooling: project setup

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  quoteProps: 'preserve',
+  jsxSingleQuote: false,
+  requirePragma: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none'
+};
